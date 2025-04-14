074a13bf31bb1c9d08473aadd9e5f70dc2cd25b9
e.ganiushina <ganiushina@verdeeslab.ru>
2025-04-14 18:55:47 +0400

    add detail fo payments

---
+++ src/main/java/ru/alta/thirdproj/entites/Salary.java
@@ -0,0 +1,25 @@
+package ru.alta.thirdproj.entites;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Salary {
+    private Double userSalaryAll;
+    private String userSalaryAllRUB;
+    private Double userBonus;
+    private String userBonusRUB;
+    private Double userBonusBDM;
+    private String userBonusBDMRUB;
+    private Double userSalaryNDFL;
+    private String userSalaryNDFLRUB;
+    private Double userSalarySingle;
+    private String userSalarySingleRUB;
+    private Double userSalary;
+    private String userSalaryRUB;
+    private Integer salaryMonth;
+    private Integer salaryQuarter;
+    private String salaryMonthStr;
+    private Integer salaryYear;
+}
