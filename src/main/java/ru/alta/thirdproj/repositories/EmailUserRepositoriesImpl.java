4d3e86acd299b0decb67f2e7855b5eee4e2c72c2
e.ganiushina <ganiushina@verdeeslab.ru>
2025-05-27 18:20:40 +0400

    fix email sending validation payment for period

--- src/main/java/ru/alta/thirdproj/repositories/EmailUserRepositoriesImpl.java
+++ src/main/java/ru/alta/thirdproj/repositories/EmailUserRepositoriesImpl.java
@@ -7,9 +7,11 @@ import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 import ru.alta.thirdproj.entites.PaymentSuccess;
 import ru.alta.thirdproj.entites.UserEmail;
+import ru.alta.thirdproj.entites.UserSalarySuccess;
 
 import java.time.LocalDate;
 import java.util.*;
+import java.util.stream.Collectors;
 
 
 @Component
@@ -25,12 +27,20 @@ public class EmailUserRepositoriesImpl {
 
     public List<UserEmail> getUserBirthday() {
         try (Connection connection = sql2o.open()) {
-            return connection.createQuery(SELECT_USER_EMAILS, false)
-                    .setColumnMappings(UserEmail.COLUMN_MAPPINGS)
-                    .executeAndFetch(UserEmail.class);
+            List<Map<String, Object>> rows = connection.createQuery(SELECT_USER_EMAILS)
+
+                    .executeAndFetchTable()
+                    .asList();
+
+            return rows.stream().map(row -> {
+                UserEmail item = new UserEmail();
+                item.setUserId((Integer) row.get("man_id"));
+                item.setUserFio((String) row.get("man_fio"));
+                item.setUserEmail((String) row.get("man_email"));
+                return item;
+            }).collect(Collectors.toList());
         }
     }
-
 
     @Transactional
     public void save(int userId , LocalDate dateFrom, LocalDate dateTo, int success)
