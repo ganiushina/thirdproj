06f7c0e5402c4141a4a6c7b14aa9c55db085befb
ganiushina <32090008+ganiushina@users.noreply.github.com>
2025-12-23 13:56:27 +0400

    Refine probation act save service

--- src/main/java/ru/alta/thirdproj/services/MarginBonusServiceImpl.java
+++ src/main/java/ru/alta/thirdproj/services/MarginBonusServiceImpl.java
@@ -2,22 +2,23 @@ package ru.alta.thirdproj.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.alta.thirdproj.entites.ActByUserCheck;
 import ru.alta.thirdproj.entites.MarginBonusBDM;
 import ru.alta.thirdproj.entites.UserSalary;
 import ru.alta.thirdproj.entites.UserSalaryDetail;
-import ru.alta.thirdproj.repositories.UserBonusKPIRepositoryImpl;
 import ru.alta.thirdproj.repositories.UserSalaryRepImplRep;
 
 import java.time.LocalDate;
 import java.util.List;
+
 @Service
 public class MarginBonusServiceImpl implements iMarginBonusService {
 
-    private UserSalaryRepImplRep userSalaryRepImplRep;
+    private final UserSalaryRepImplRep userSalaryRepImplRep;
 
     @Autowired
-    public void setMarginBonusServiceImpl(UserSalaryRepImplRep userSalaryRepImplRep){
+    public MarginBonusServiceImpl(UserSalaryRepImplRep userSalaryRepImplRep) {
         this.userSalaryRepImplRep = userSalaryRepImplRep;
     }
 
@@ -42,6 +43,7 @@ public class MarginBonusServiceImpl implements iMarginBonusService {
     }
 
     @Override
+    @Transactional
     public void saveFailedProbationAct(Integer actId, Double totalNoNds, String candidate,
                                        List<ActByUserCheck> participants) {
         userSalaryRepImplRep.saveFailedProbationAct(actId, totalNoNds, candidate, participants);
